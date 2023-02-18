e13d6fb5a4d657e78910586426bf5d0d6ec9bb10
Amir Salehi <iamamirsalehi@gmail.com>
2023-02-18 18:59:31 +0330

    index.php updated

--- index.php
+++ index.php
@@ -4,13 +4,21 @@ use src\Basket\Basket;
 use Src\Product;
 use Src\Bundle;
 
-$phpCourse = Product::new('PHP Course', 3000000, 5);
-$jsCourse = Product::new('Js Course', 2000000, 2);
-$wordpressCourse = Product::new('Wordpress Course', 1000000, 3);
-$uiCourse = Product::new('UI Course', 1200000, 8);
+try {
 
-$topTenCourses = new Bundle();
-$topTenCourses->addProduct($phpCourse)->addProduct($jsCourse)->addProduct($wordpressCourse);
 
-$basket = new Basket();
-$basket->addItem($topTenCourses)->addItem($uiCourse);
+    $phpCourse = Product::new('PHP Course', 3000000, 5);
+    $jsCourse = Product::new('Js Course', 2000000, 2);
+    $wordpressCourse = Product::new('Wordpress Course', 1000000, 3);
+    $uiCourse = Product::new('UI Course', 1200000, 8);
+
+    $topTenCourses = new Bundle();
+    $topTenCourses->addProduct($phpCourse)->addProduct($jsCourse)->addProduct($wordpressCourse)->setDiscount(10);
+
+    $basket = new Basket();
+    $basket->addItem($topTenCourses)->addItem($uiCourse);
+
+
+} catch (\Src\Exceptions\BusinessException $exception) {
+    echo sprintf("\n --- %s --- \n", $exception->getMessage());
+}
--- src/Bundle.php
+++ src/Bundle.php
@@ -2,9 +2,12 @@
 
 namespace Src;
 
+use Src\Exceptions\Business\ProductApplicationException;
+
 class Bundle
 {
     private array $products = [];
+    private float $discount = 0;
 
     public function addProduct(Product $product): self
     {
@@ -13,8 +16,31 @@ class Bundle
         return $this;
     }
 
+    public function setDiscount(float $discount = 0): self
+    {
+        if ($discount < 0 or $discount > 100) {
+            throw ProductApplicationException::discountMustBeBetweenZeroAndHundred();
+        }
+
+        $this->discount = $discount;
+
+        return $this;
+    }
+
     public function getProducts(): array
     {
         return $this->products;
     }
+
+    public function getTotalPrice(): int
+    {
+        $totalPrice = 0;
+
+        foreach ($this->products as $product) {
+            $totalPrice += $product->getPrice();
+        }
+
+        return $totalPrice;
+    }
+
 }
